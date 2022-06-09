27072d0278fdb5d92139bf6e54a659a4e7ad4d0d
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-06-09 08:56:44 +0100

    Update Post model

--- src/apis/posts/model.js
+++ src/apis/posts/model.js
@@ -5,8 +5,8 @@ const { Schema, model } = mongoose;
 const postSchema = new Schema(
   {
     text: { type: String, required: true },
-    username: { type: String, required: true },
-    image: { type: String, required: true },
+    username: { type: String },
+    image: { type: String },
     user: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
   },
   { timestamps: true }
